b4f409a3621e5f225d84fb1a3fda518036ec0e34
Pradeum Kumar Swarnkar <iamswarnkar7@gmail.com>
2022-10-07 13:04:49 +0530

    frist commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function App(){
+
+  const [data, setData] = useState();
+
+  function apiCalling(){
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then((res)=>setData(res.data))
+    .catch((err)=>console.log(err))
+  }
+  useEffect(()=>{
+    apiCalling();
+  },[])
+  console.log(data)
+  return (
+    <>
+      <ul>  
+        {
+          data.map((item)=>{
+            return <li key={item.key}>{item.title}</li>
+          })
+        }
+      </ul>
+    </>
+  )
+}
